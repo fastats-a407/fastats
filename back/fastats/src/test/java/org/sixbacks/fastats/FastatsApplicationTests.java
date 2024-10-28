9410174f1a405252a92adc8dd874bf565407b5f0
glenn.syj <glenn.syj@gmail.com>
2024-10-28 14:19:05 +0900

    init: 레포지토리 초기화 및 백엔드 프로젝트 생성
    S11P31A407-133

---
+++ back/fastats/src/test/java/org/sixbacks/fastats/FastatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sixbacks.fastats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
