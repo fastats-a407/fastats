37a09a154f5a13ceb57d38f33af8f2584c5e93c4
glenn.syj <glenn.syj@gmail.com>
2024-10-29 16:48:45 +0900

    feat: API 응답 및 에러 관련 Global 환경 구성
    공통 API 응답 클래스 구현
    CustomException 및 핸들러 구현
    
    S11P31A407-182

---
+++ back/fastats/src/main/java/org/sixbacks/fastats/global/error/ErrorCode.java
@@ -0,0 +1,25 @@
+package org.sixbacks.fastats.global.error;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+	/*
+		TODO: 개발 과정에서 지속적인 추가 필요
+		NOTE: {NAME}(status, code, message)
+	 */
+
+	STAT_ILL_REQUEST(HttpStatus.BAD_REQUEST, 1101, "유효하지 않은 통계표 요청입니다");
+
+	private final HttpStatus status;
+	private final int code;
+	private final String message;
+
+}
+
+
