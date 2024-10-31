783751be0e19eafb1fc7eea52e5885da9bd16a71
송창용 <krca5876@naver.com>
2024-10-31 10:35:40 +0900

    feat: docker-compose 파일 작성

---
+++ infra/init.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS fastats;
+USE fastats;
+
+DROP TABLE IF EXISTS `sector`;
+
+CREATE TABLE `sector` (
+    `id`    INT    NOT NULL AUTO_INCREMENT,
+    `code`    VARCHAR(4)    NOT NULL,
+    `description`    VARCHAR(16)    NULL,
+    CONSTRAINT `PK_SECTOR` PRIMARY KEY (`id`)
+);
+
+DROP TABLE IF EXISTS `stat_survey`;
+
+CREATE TABLE `stat_survey` (
+    `id`    INT    NOT NULL AUTO_INCREMENT,
+    `sector_id`    INT    NOT NULL,
+    `org_code`    INT    NOT NULL,
+    `org_name`    VARCHAR(64)    NOT NULL,
+    `name`    VARCHAR(64)    NOT NULL,
+    `coll_start_date`    VARCHAR(8)    NULL,
+    `coll_end_date`    VARCHAR(8)    NULL,
+    `coll_period`    VARCHAR(4)    NULL,
+    CONSTRAINT `PK_STAT_SURVEY` PRIMARY KEY (`id`),
+    CONSTRAINT `FK_sector_TO_stat_survey_1` FOREIGN KEY (`sector_id`) REFERENCES `sector` (`id`)
+);
+
+DROP TABLE IF EXISTS `stat_table`;
+
+CREATE TABLE `stat_table` (
+    `id`    BIGINT    NOT NULL AUTO_INCREMENT,
+    `survey_id`    INT    NOT NULL,
+    `name`    VARCHAR(255)    NOT NULL,
+    `content`    TEXT    NOT NULL,
+    `comment`    TEXT    NULL,
+    `kosis_tb_id`    VARCHAR(64)    NOT NULL,
+    `kosis_view_link`    TEXT    NULL,
+    CONSTRAINT `PK_STAT_TABLE` PRIMARY KEY (`id`),
+    CONSTRAINT `FK_stat_survey_TO_stat_table_1` FOREIGN KEY (`survey_id`) REFERENCES `stat_survey` (`id`)
+);
