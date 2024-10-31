27b76619cc01b00a996c7643cf95ee96b304ddec
JuYoung Lee <rsb987@naver.com>
2024-10-31 16:48:25 +0900

    feat: repository 메소드 추가 엔티티 저장을 위한 메소드 추가
    S11P31A407-115

--- back/fastats/src/main/java/org/sixbacks/fastats/statistics/repository/SectorRepository.java
+++ back/fastats/src/main/java/org/sixbacks/fastats/statistics/repository/SectorRepository.java
@@ -1,8 +1,16 @@
 package org.sixbacks.fastats.statistics.repository;
 
+import java.util.Optional;
+
+import org.sixbacks.fastats.statistics.entity.Sector;
+import org.springframework.stereotype.Repository;
+
 /*
 	NOTE: 기능을 정의하기 위해 이용
  */
+@Repository
 public interface SectorRepository {
+	Sector save(Sector sector);
 
+	Optional<Sector> findByCode(String code);
 }
