6af91c81403b1973493d2db58201b5aaaf95943f
허동민 <gjehdals456@gmail.com>
2024-11-04 17:56:55 +0900

    feat : 테이블 변화에 따른 쿼리문 수정 / 테스트 코드 보완
    3중조인에서 4중조인으로 변경

--- back/fastats/src/test/java/org/sixbacks/fastats/statistics/service/ElasticSearchServiceImplTest.java
+++ back/fastats/src/test/java/org/sixbacks/fastats/statistics/service/ElasticSearchServiceImplTest.java
@@ -1,12 +1,13 @@
 package org.sixbacks.fastats.statistics.service;
 
+import java.util.stream.StreamSupport;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.sixbacks.fastats.statistics.dto.document.StatDataDocument;
 import org.sixbacks.fastats.statistics.repository.jdbc.StatSurveyJdbcRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
@@ -21,19 +22,29 @@ public class ElasticSearchServiceImplTest {
 	@Autowired
 	private ElasticSearchService elasticSearchService;
 
-	@MockBean
+	@Autowired
 	private StatSurveyJdbcRepository statSurveyJdbcRepository;
 
 	@Autowired
 	private ElasticsearchRepository<StatDataDocument, Long> elasticsearchRepository;
 
 	@Test
-	public void testSaveData() {
+	public void testSaveData() throws InterruptedException {
 		// saveData 호출
 		elasticSearchService.saveData();
 
+		// 데이터 저장 완료 후 약간의 지연 시간 주기
+		Thread.sleep(1000);
+
 		// Elasticsearch에서 데이터가 저장되었는지 확인
 		Iterable<StatDataDocument> responses = elasticsearchRepository.findAll();
-		System.out.println(responses.iterator().next());
+
+		// 저장된 데이터 출력
+		responses.forEach(System.out::println);
+
+		// 데이터가 들어갔는지 개수를 확인
+		long count = StreamSupport.stream(responses.spliterator(), false).count();
+		System.out.println("Stored documents count: " + count);
 	}
+
 }
