724162ee99b539c2c543f029459b609da3841739
허동민 <gjehdals456@gmail.com>
2024-11-04 15:41:24 +0900

    feat : 테이블 변화에 따른 쿼리문 수정
    3중조인에서 4중조인으로 변경

--- back/fastats/src/main/java/org/sixbacks/fastats/statistics/repository/jdbc/StatSurveyJdbcRepository.java
+++ back/fastats/src/main/java/org/sixbacks/fastats/statistics/repository/jdbc/StatSurveyJdbcRepository.java
@@ -1,7 +1,6 @@
 package org.sixbacks.fastats.statistics.repository.jdbc;
 
 import java.util.List;
-
 import org.sixbacks.fastats.statistics.dto.document.StatDataDocument;
 import org.sixbacks.fastats.statistics.entity.StatSurvey;
 import org.sixbacks.fastats.statistics.repository.StatSurveyRepository;
@@ -9,22 +8,26 @@ import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface StatSurveyJdbcRepository extends StatSurveyRepository, ListCrudRepository<StatSurvey, Long> {
+
 	@Query("""
-		SELECT
-		                                           ss.name AS stat_survey_name,
-		                                           ss.org_name AS stat_org_name,
-		                                           st.name AS stat_table_name,
-		                                           st.content AS stat_table_content,
-		                                           st.comment AS stat_table_comment,
-		                                           st.kosis_view_link AS stat_table_kosis_view_link,
-		                                           ci.start_date AS coll_info_start_date,
-		                                           ci.end_date AS coll_info_end_date
-		                                       FROM
-		                                           stat_survey ss
-		                                       JOIN
-		                                           stat_table st ON ss.id = st.survey_id
-		                                       JOIN
-		                                           coll_info ci ON st.id = ci.stat_table_id
-		""")
+        SELECT
+            ss.name AS stat_survey_name,
+            so.name AS stat_org_name,
+            st.name AS stat_table_name,
+            st.content AS stat_table_content,
+            st.comment AS stat_table_comment,
+            st.kosis_view_link AS stat_table_kosis_view_link,
+            ci.start_date AS coll_info_start_date,
+            ci.end_date AS coll_info_end_date
+        FROM
+            stat_survey ss
+        JOIN
+            stat_table st ON ss.id = st.survey_id
+        JOIN
+            coll_info ci ON st.id = ci.stat_table_id
+        JOIN
+            stat_org so ON ss.org_id = so.id
+        """
+	)
 	List<StatDataDocument> findAllStatData();
 }
