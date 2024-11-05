bd3bd6c606fa90f2bb323b3bcb859523addd1d15
glenn.syj <glenn.syj@gmail.com>
2024-11-05 14:39:12 +0900

    fix: ElasticSearchRepository 내 불필요한 쿼리 삭제
    S11P31A407-200

--- back/fastats/src/main/java/org/sixbacks/fastats/statistics/repository/jdbc/ElasticSearchRepository.java
+++ back/fastats/src/main/java/org/sixbacks/fastats/statistics/repository/jdbc/ElasticSearchRepository.java
@@ -1,18 +1,8 @@
 package org.sixbacks.fastats.statistics.repository.jdbc;
 
 import org.sixbacks.fastats.statistics.dto.document.StatDataDocument;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface ElasticSearchRepository extends ElasticsearchRepository<StatDataDocument, Long> {
-
-	@Query("{ \"multi_match\": { " +
-		"  \"query\": \"?0\"," +
-		"  \"fields\": [\"statSurveyName\", \"statOrgName\", \"statTableName\", \"statTableContent\"]," +
-		"  \"type\": \"best_fields\"" +
-		"} }")
-	Page<StatDataDocument> searchByKeyword(String keyword, Pageable pageable);
 
 }
