b55ccd5af524db274cc1d1ec6af5a504438d70d4
허동민 <gjehdals456@gmail.com>
2024-11-08 14:07:42 +0900

    feat : Docuemnt 경로 변경 및 Sector 필드 추가
    5중 조인으로 Repository 변경

--- back/fastats/src/main/java/org/sixbacks/fastats/statistics/repository/jdbc/ElasticSearchRepository.java
+++ back/fastats/src/main/java/org/sixbacks/fastats/statistics/repository/jdbc/ElasticSearchRepository.java
@@ -1,6 +1,6 @@
 package org.sixbacks.fastats.statistics.repository.jdbc;
 
-import org.sixbacks.fastats.statistics.dto.document.StatDataDocument;
+import org.sixbacks.fastats.statistics.entity.document.StatDataDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface ElasticSearchRepository extends ElasticsearchRepository<StatDataDocument, Long> {
