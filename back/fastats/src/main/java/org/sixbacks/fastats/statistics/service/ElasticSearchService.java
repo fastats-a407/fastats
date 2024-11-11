ebdac7c8b756bb8d807944980918cd7fe39fa23a
glenn.syj <glenn.syj@gmail.com>
2024-11-11 14:19:01 +0900

    feat: ES 카테고리 집계를 위한 도큐먼트 및 서비스 수정
    S11P31A407-252

--- back/fastats/src/main/java/org/sixbacks/fastats/statistics/service/ElasticSearchService.java
+++ back/fastats/src/main/java/org/sixbacks/fastats/statistics/service/ElasticSearchService.java
@@ -1,5 +1,8 @@
 package org.sixbacks.fastats.statistics.service;
 
+import java.util.List;
+
+import org.sixbacks.fastats.statistics.dto.response.CategoryListResponse;
 import org.sixbacks.fastats.statistics.dto.response.StatTableListResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.elasticsearch.core.query.Query;
@@ -16,4 +19,8 @@ public interface ElasticSearchService {
 	Page<StatTableListResponse> searchByKeyword(String keyword, int page, int size);
 
 	Page<StatTableListResponse> searchByKeyword(String keyword, int page, int size, Query query);
+
+	CategoryListResponse getCategoriesByKeyword(String keyword);
+
+	CategoryListResponse getCategoriesByKeyword(String keyword, List<String> aggrList, Query query);
 }
