1a987a9818ede75b771ae1afe477d6c4015eeca8
JuYoung Lee <rsb987@naver.com>
2024-11-11 17:23:52 +0900

    feat: 메인 페이지 작성 검색창, 키워드 화면 구성
    S11P31A407-260

---
+++ front/fast/app/components/SearchResults.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React from 'react'
+import { SuggestionKeyword } from './type'
+
+
+interface SearchResultsProps {
+    statistics: SuggestionKeyword[];
+    searching: boolean;
+    hidden: boolean;
+    onMouseOver: React.MouseEventHandler<HTMLLIElement>;
+    onMouseLeave: React.MouseEventHandler<HTMLLIElement>;
+    onClick: React.MouseEventHandler<HTMLLIElement>;
+}
+
+
+export default function SearchResults({ statistics, searching, hidden, onMouseOver, onMouseLeave, onClick }: SearchResultsProps) {
+    if (hidden) return null;
+    return (
+        <article aria-busy={searching}>
+            {searching ? (
+                '잠시만 기다려주세요.'
+            ) : (
+                <>
+                    <ul>
+                        {/* {statistics.map(({ id, keyword }) => (
+                            <li key={id} onMouseLeave={onMouseLeave} onMouseOver={onMouseOver} onClick={onClick}>
+                                {keyword}
+                            </li>
+                        ))} */}
+                        {statistics.map(({ id, keyword }) => (
+                            <li key={id} onMouseLeave={onMouseLeave} onMouseOver={onMouseOver} onClick={onClick}>
+                                {keyword}
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            )}
+        </article>
+    );
+}
---
+++ front/fast/app/lib/Search.ts
@@ -0,0 +1,6 @@
+
+
+
+export async function useSearch() {
+    return new Promise(resolve => setTimeout(resolve, 500))
+}
