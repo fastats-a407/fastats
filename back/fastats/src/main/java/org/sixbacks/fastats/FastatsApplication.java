23a2385c4b1f4331d615f35017f7a4c907d00a50
qnsghdharo119 <qnsghdharo119@naver.com>
2024-11-13 11:27:14 +0900

    remove: Delete unnecessary import

--- back/fastats/src/main/java/org/sixbacks/fastats/FastatsApplication.java
+++ back/fastats/src/main/java/org/sixbacks/fastats/FastatsApplication.java
@@ -3,8 +3,6 @@ package org.sixbacks.fastats;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
 @SpringBootApplication
 public class FastatsApplication {
 
